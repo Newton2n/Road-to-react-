6cf7e912075290ab22df0c863ed72fea090310b2
Newton <newtonyt04@gmail.com>
2025-09-16 22:23:19 +0600

    counter context project complete and cleaner code

--- 08.1counterContext/src/Components/Button.jsx
+++ 08.1counterContext/src/Components/Button.jsx
@@ -1,20 +1,20 @@
-import {React,useContext} from 'react'
-import  {CounterContext} from '../Context/CountContext'
+import { React, useContext } from "react";
+import { CounterContext } from "../Context/CountContext";
 function Button() {
-    const value = useContext(CounterContext)
-    console.log(value.Count);
+  const value = useContext(CounterContext);
+
   return (
     <div>
-        
-        <button onClick={()=>{ value.setCount( value.count+1) }}>
-            
-            i am second Button {value.count}
-        </button>
-        <p>{value.count}</p>
-        
+      <button
+        onClick={() => {
+          value.setCount(value.count + 1);
+        }}
+      >
+        i am second Button {value.count}
+      </button>
+      <p>{value.count}</p>
     </div>
-  )
-  
+  );
 }
 
-export default Button
+export default Button;
